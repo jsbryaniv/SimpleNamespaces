d9c6d4def2509cf952d315428ef312d6cdda73da
Shep Bryan IV <jsbryan4@asu.edu>
2022-08-15 15:14:16 -0700

    Initialized

---
+++ src/SimpleNamespaces.jl
@@ -0,0 +1,11 @@
+module SimpleNamespaces
+
+export SimpleNamespace
+
+function greet()
+    println("Hello")
+end
+
+include("simplenamespace.jl")
+
+end
---
+++ src/simplenamespace.jl
@@ -0,0 +1,41 @@
+
+"""
+SimpleNamespaces creates SimpleNamespace objects.
+SimpleNamespace is a dictionary wrapper that allows dot indexing.
+For example:
+    > x = SimpleNamespace(a=1, b=2)
+    > x.c = "hello"
+    > print(x.a, x.b, x.c)
+    12hello 
+"""
+
+### SimpleNamespace ### 
+struct SimpleNamespace
+    properties::Dict{String, Any}
+end
+SimpleNamespace(properties) = SimpleNamespace(Dict{String, Any}([(String(key), val) for (key, val) in pairs(properties)]))
+SimpleNamespace(;kwargs...) = SimpleNamespace(kwargs)
+
+# Allow dot indexing to access properties
+Base.getproperty(x::SimpleNamespace, property::Symbol) = getfield(x, :properties)[String(property)]
+Base.setproperty!(x::SimpleNamespace, property::Symbol, value) = getfield(x, :properties)[String(property)] = value
+
+# Allow property names and keys to be accessed using Base functions
+Base.propertynames(x::SimpleNamespace) = keys(getfield(x, :properties))
+Base.keys(x::SimpleNamespace) = keys(getfield(x, :properties))
+
+# Allow property keys and values to be accessed using Base functions
+Base.pairs(x::SimpleNamespace) = pairs(getfield(x, :properties))
+
+# Allow for simple conversion to dictionary
+Base.Dict(x::SimpleNamespace) = getfield(x, :properties)
+
+# Define merge function
+Base.merge(x::SimpleNamespace, others::SimpleNamespace...) = SimpleNamespace(merge(Dict(x), Dict.(others)...))
+
+# Define copy functions
+Base.copy(x::SimpleNamespace) = SimpleNamespace(copy(Dict(x)))
+Base.deepcopy(x::SimpleNamespace) = SimpleNamespace(deepcopy(Dict(x)))
+
+
+
